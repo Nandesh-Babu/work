c8e82dcdf0dbe3d0b73abeeb94029e87dbe45343
Nandesh-Babu <nandeshbabu@gmail.com>
2023-09-05 17:42:24 +0530

    new file

--- Python/Divide_two_num_without_div_ops.py
+++ Python/Divide_two_num_without_div_ops.py
@@ -2,10 +2,20 @@ import math
 
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        op = 1 if divisor > 0 else -1
+        if divisor < 0 and dividend < 0:
+            op = 1 
+        elif divisor < 0 or dividend < 0:
+            op = -1
+        else:
+            op = 1
+        quotient = 0  
         dividend = abs(dividend)
         divisor = abs(divisor)
-        quotient = 0  
+        if dividend==1 and divisor==1:
+            quotient = 1
+            return math.trunc(quotient * op) 
+        elif divisor == 1:
+            return dividend * op
         while dividend >= divisor:
             dividend -= divisor
             quotient += 1 
