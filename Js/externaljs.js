493893bc419fe3f3b8ac61c4d5e3f6f0a9abec5e
Nandesh@31 <nandeshbabu@gmail.com>
2025-01-06 19:03:37 +0530

    javascript samples

---
+++ Js/externaljs.js
@@ -0,0 +1,26 @@
+function myFunction(){
+        document.getElementById("demo").innerHTML="Paragraph changed.";
+    }
+
+document.write(0+0) 
+
+window.alert("hey this is just dummy work")
+
+console.log("file run success")
+
+var x,y,z;
+x=5;
+y=1; z=x+y;
+
+document.getElementById("cal").innerHTML = "The value of z is " + z + ".";
+
+const person = new Object();
+
+person.name="Nandesh"; person.age= 24; person.city = "Madurai";
+
+let text = "";
+for (let x in person){
+    text += person[x] + " ";
+};
+
+document.getElementById("loop").innerHTML = text;
